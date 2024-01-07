2fed698f471bd28260de3441b0599624f558d3af
datahaikuninja <jundai.takashina@gmail.com>
2024-01-07 18:18:32 +0900

    add new keymap: switch workspace

--- wezterm.lua
+++ wezterm.lua
@@ -106,7 +106,31 @@ config.keys = {
         -- switch workspace
         key = 's',
         mods = 'LEADER',
-        action = act.ShowLauncherArgs { flags = 'WORKSPACES' },
+        action = wezterm.action_callback (function (window, pane)
+            -- create workspace list
+            local workspaces = {}
+            for i, name in ipairs(wezterm.mux.get_workspace_names()) do
+                table.insert(workspaces, {
+                    id = name,
+                    label = string.format('%d: %s', i, name),
+                })
+            end
+            local current = wezterm.mux.get_active_workspace()
+            -- raunch selection dialog
+            window:perform_action(act.InputSelector {
+                action = wezterm.action_callback (function (_, _, id, label)
+                    if not id and not label then
+                        wezterm.log_info 'Workspace selection canceled'
+                    else
+                        window:perform_action(act.SwitchToWorkspace { name = id }, pane) --switch to workspace
+                    end
+                end),
+                title = 'Select workspace',
+                choices = workspaces,
+                fuzzy = true,
+                fuzzy_description = string.format('Select workspace: %s -> ', current),
+            }, pane)
+        end),
     },
     {
         key = '$',
