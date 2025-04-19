15a2d7229e7f0edd78c13fe13c93f7988d676360
datahaikuninja <jundai.takashina@gmail.com>
2025-04-19 11:22:45 +0900

    fix: modify font size

--- wezterm.lua
+++ wezterm.lua
@@ -49,7 +49,7 @@ config.font = wezterm.font_with_fallback({
 })
 
 -- font_size
-config.font_size = 16.0
+config.font_size = 14.0
 
 -- leader key
 config.leader = { key = "/", mods = "CTRL", timeout_milliseconds = 1000 }
